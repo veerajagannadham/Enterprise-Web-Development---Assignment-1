5fd30ef6e5d381fd5ac7dfc4d38c80c295e5b771
veerajaganadham <160045450+veerajagannadham@users.noreply.github.com>
2025-05-04 03:12:44 +0530

    New Changes Added

---
+++ test/movie-reviews-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MovieReviewsApi from '../lib/movie-reviews-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/movie-reviews-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MovieReviewsApi.MovieReviewsApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
