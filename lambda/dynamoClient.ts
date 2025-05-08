205366941f8497499b971654b3c79149f26f0814
veerajagannadham <160045450+veerajagannadham@users.noreply.github.com>
2025-05-09 03:22:16 +0530

    Initial Commit

--- lambda/dynamoClient.ts
+++
@@ -1,5 +0,0 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-
-const dbClient = new DynamoDBClient({});
-export const dynamoDb = DynamoDBDocumentClient.from(dbClient);
--- lambda/reviewTypes.ts
+++
@@ -1,8 +0,0 @@
-export interface Review {
-    movieId: number;  // Foreign key (links to movies.ts)
-    reviewId: number; // Unique review ID
-    reviewerId: string; // Email of the reviewer
-    reviewDate: string; // Date in YYYY-MM-DD format
-    content: string; // Review text
-  }
-  
